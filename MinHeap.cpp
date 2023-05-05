ca2a2bfd921a7b8dc7c334afd712f61fdb042377
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-05-05 21:12:06 +0300

    Added Sort and Dep Report

--- MinHeap.cpp
+++ MinHeap.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <queue>
+#include <map>
+#include <utility>
 using namespace std;
 
 
@@ -35,15 +36,15 @@ public:
         }
     }
     void insert(Student* array, Student NewS){
-        array[length] = NewS;
+        array[length] = std::move(NewS);
         length++;
         for (int i = length/2 - 1; i >= 0; --i) {
             MinHeapify(array, i);
         }
     }
-    Student* remove(Student* array, Student S) {
+    Student* remove(Student* array, const Student& S) {
         bool found = false;
-        int i = 0;
+        int i;
         for (i = 0; i < length; ++i) {
             if (array[i].id == S.id) {
                 found = true;
@@ -64,48 +65,69 @@ public:
         }
         return array;
     }
-
+    void SortHeap(Student* array){
+        int SortedL = -1;
+        int L = length;
+        Student* SortedStudents = new Student[length];
+        for (int i = 0; i < L; ++i) {
+            SortedStudents[++SortedL] = array[0];
+            remove(array, array[0]);
+        }
+        for (int i = 0; i <= SortedL; ++i) {
+            array[i] = SortedStudents[i];
+            length++;
+        }
+    }
+    void Dep_Report(Student* array) const{
+        map<string, int> Dep_Stud;
+        for (int i = 0; i < length; ++i) {
+            Dep_Stud[array[i].Dep]++;
+        }
+        for (auto & i : Dep_Stud) {
+            cout << i.first << " Department Has " << i.second << " Students\n";
+        }
+    }
 };
 
 
 int main(){
     MinHeap myMinH;
-    Student* students = new Student[10];
+    Student students[10];
     Student s1;
-    s1.Name = "Noha";
+    s1.Name = "Mariam";
     s1.Dep = "IS";
-    s1.GPA = 0.2;
-    s1.id = 20210001;
+    s1.GPA = 0.99;
+    s1.id = 1;
     Student s2;
-    s2.Name = "Naemat";
+    s2.Name = "Aya";
     s2.Dep = "IS";
-    s2.GPA = 0.1;
-    s2.id = 20210002;
+    s2.GPA = 3.77;
+    s2.id = 2;
     Student s3;
-    s3.Name = "Ramly";
+    s3.Name = "Habiba";
     s3.Dep = "DS";
-    s3.GPA = 1.69;
-    s3.id = 20210003;
+    s3.GPA = 2.69;
+    s3.id = 3;
     Student s4;
     s4.Name = "Khaled";
     s4.Dep = "AI";
     s4.GPA = 3.12;
-    s4.id = 20210004;
+    s4.id = 4;
     Student s5;
     s5.Name = "Mohammed";
     s5.Dep = "DS";
-    s5.GPA = 1.23;
-    s5.id = 20210005;
+    s5.GPA = 1.93;
+    s5.id = 5;
     Student s6;
     s6.Name = "Osama";
     s6.Dep = "CS";
-    s6.GPA = 3.69;
-    s6.id = 20210006;
+    s6.GPA = 0.69;
+    s6.id = 6;
     Student s7;
     s7.Name = "Ahmed";
     s7.Dep = "AI";
     s7.GPA = 1.65;
-    s7.id = 20210007;
+    s7.id = 7;
     myMinH.insert(students, s1);
     myMinH.insert(students, s2);
     myMinH.insert(students, s3);
@@ -117,8 +139,15 @@ int main(){
         cout << "Student #" << i+1 << '\n' << students[i] << '\n';
     }
     cout << '\n';
-    myMinH.remove(students, s2);
+    cout << "#################################\n";
+
+    myMinH.remove(students, s5);
     for (int i = 0; i < myMinH.length; ++i) {
+        cout << "Student #" << i+1 << '\n' << students[i] << '\n';
+    }
+    cout << "#################################\n";
+    myMinH.SortHeap(students);
+    for (int i = 0; i < myMinH.length; ++i){
         cout << "Student #" << i+1 << '\n' << students[i] << '\n';
     }
 }
