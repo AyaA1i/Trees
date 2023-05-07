6ff02e6ec375cffca15350902ce598ceaca5ac97
Aya Ali <101468593+AyaA1i@users.noreply.github.com>
2023-05-07 23:05:16 +0300

    Update and rename BST.CPP to BST.h

---
+++ BST.h
@@ -0,0 +1,139 @@
+#ifndef ASSIGN33_BST_H
+#define ASSIGN33_BST_H
+#include "Student.h"
+#include "Node.h"
+class BST {
+public:
+    Node* root = nullptr;
+    // (left -> root -> right)
+    void Traverse(Node* p ) {
+        if(p!=nullptr){
+            Traverse(p->left);
+            cout<<p->student;
+            Traverse(p->right);
+        }
+    }
+
+    //Insertion in a binary search tree
+    Node* search(int el) const{
+        Node* cur = root;
+        while(cur != nullptr && cur->student.id != el){
+            if(el < cur ->student.id){
+                cur = cur ->left;
+            }else{
+                cur = cur->right;
+            }
+        }
+        if(cur != nullptr && cur->student.id == el){
+            return cur;
+        }
+        return nullptr;
+    }
+    //Insertion in a binary search tree
+    void insert(Student stu){
+        Node* cur = root;
+        Node* pr = nullptr;
+        while(cur != nullptr )
+        {
+            pr = cur;
+            if(stu.id < cur ->student.id)
+            {
+                cur = cur ->left;
+            }
+            else
+            {
+                cur = cur->right;
+            }
+        }
+        Node* newnode = new Node();
+        newnode->student = stu ;
+        newnode->right = nullptr;
+        newnode->left = nullptr;
+        //if tree is empty
+        if(pr == nullptr)
+        {
+            root = newnode;
+        }
+        else if(pr-> student.id < stu.id  )
+        {
+            pr->right =  newnode;
+        }
+        else
+        {
+            pr->left =newnode;
+        }
+    }
+    //Deletion in a binary search tree
+    void Delete(Node *node)
+    {
+        //get node prev
+        Node* prev = root;
+        while((prev->left != node && prev->right != node) && prev != node){
+            if(node->student.id < prev ->student.id){
+                prev = prev ->left;
+            }else{
+                prev = prev->right;
+            }
+        }
+        //node is a leaf
+        if(node->left == nullptr && node->right == nullptr )
+        {
+            if(prev->left == node )
+            {
+                prev -> left = nullptr;
+            }
+            else
+            {
+                prev -> right = nullptr;
+            }
+            return;
+        }
+        //node has no left child
+        if(node->left == nullptr)
+        {
+            node->student.id = node->right->student.id;
+            node->left = node->right->left;
+            node->right = node->right->right;
+        }
+            //node has no right child
+        else if(node->right == nullptr)
+        {
+            node->student.id= node->left->student.id;
+            node->right = node->left->right;
+            node->left = node->left->left;
+        }
+            //node has two children
+        else
+        {
+            Node* p = node->right;
+            while(p -> left!=nullptr){
+                p = p->left;
+            }
+            Node* prevp = node->right;
+            while(prevp->left != p){
+                prevp = prevp->left;
+            }
+            node -> student.id = p->student.id;
+            prevp -> left = nullptr;
+
+
+        }
+
+    }
+    void departmentReport(Node *node) {
+        map<string, int> count;
+        countStudentsPerDepartment(node, count);
+        for (auto [department, count] : count) {
+            cout << department << ": " << count << " students." << endl;
+        }
+    }
+    void countStudentsPerDepartment(Node *node, map<string, int>& count) {
+        if (node != NULL) {
+            countStudentsPerDepartment(node->left, count);
+            count[node->student.department]++;
+            countStudentsPerDepartment(node->right, count);
+        }
+    }
+};
+
+#endif //ASSIGN33_BST_H
