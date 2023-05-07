1066b342d8d8b4f83f4136a5cd4cea872bf37b48
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-05-08 00:24:24 +0300

    Update MinHeap.h

--- MinHeap.h
+++ MinHeap.h
@@ -1,76 +1,84 @@
-//#ifndef ASSIGN33_MINHEAP_H
-//#define ASSIGN33_MINHEAP_H
-//#include "Student.h"
-//class MinHeap {
-//public:
-//    int length = 0;
-//    Student *array;
-//    void MinHeapify(Student* array, int i){
-//        int left = 2*i + 1;
-//        int right = 2*i + 2;
-//        int min = i;
-//        if(left < length && array[min].GPA > array[left].GPA){
-//            min = left;
-//        }
-//        else if(right < length && array[min].GPA > array[right].GPA){
-//            min = right;
-//        }
-//        if(min != i){
-//            swap(array[min], array[i]);
-//            MinHeapify(array, min);
-//        }
-//    }
-//    void AddStudent(Student NewS){
-//        array[length] = std::move(NewS);
-//        length++;
-//        for (int i = length/2 - 1; i >= 0; --i) {
-//            MinHeapify(array, i);
-//        }
-//    }
-//    Student* remove(Student* array, const Student& S) {
-//        bool found = false;
-//        int i;
-//        for (i = 0; i < length; ++i) {
-//            if (array[i].id == S.id) {
-//                found = true;
-//                break;
-//            }
-//        }
-//        if (found) {
-//            array[i] = array[length - 1];
-//            length--;
-//            MinHeapify(array, i);
-//            for (int j = length/2 - 1; j >= 0; j--)
-//            {
-//                MinHeapify(array, j);
-//            }
-//        }
-//        else {
-//            cout << "Student Not Found";
-//        }
-//        return array;
-//    }
-//    void SortHeap(){
-//        int SortedL = -1;
-//        int L = length;
-//        Student* SortedStudents = new Student[length];
-//        for (int i = 0; i < L; ++i) {
-//            SortedStudents[++SortedL] = array[0];
-//            remove(array, array[0]);
-//        }
-//        for (int i = 0; i <= SortedL; ++i) {
-//            array[i] = SortedStudents[i];
-//            length++;
-//        }
-//    }
-//    void Dep_Report(Student* array) const{
-//        map<string, int> Dep_Stud;
-//        for (int i = 0; i < length; ++i) {
-//            Dep_Stud[array[i].department]++;
-//        }
-//        for (auto & i : Dep_Stud) {
-//            cout << i.first << " Department Has " << i.second << " Students\n";
-//        }
-//    }
-//};
-//#endif //ASSIGN33_MINHEAP_H
+#ifndef ASSIGNMENT_3_MINHEAP_H
+#define ASSIGNMENT_3_MINHEAP_H
+#include "Student.h"
+class MinHeap {
+public:
+    int length = 0;
+    Student* array = new Student[100];
+    void MinHeapify(int i){
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+        int min = i;
+        if(left < length && array[min].GPA > array[left].GPA){
+            min = left;
+        }
+        else if(right < length && array[min].GPA > array[right].GPA){
+            min = right;
+        }
+        if(min != i){
+            swap(array[min], array[i]);
+            MinHeapify(min);
+        }
+    }
+    void AddStudent(Student NewS){
+        array[length] = std::move(NewS);
+        length++;
+        for (int i = length/2 - 1; i >= 0; --i) {
+            MinHeapify(i);
+        }
+    }
+    Student* RemoveStudent(int id) {
+        bool found = false;
+        int i;
+        for (i = 0; i < length; ++i) {
+            if (array[i].id == id) {
+                found = true;
+                break;
+            }
+        }
+        if (found) {
+            array[i] = array[length - 1];
+            length--;
+            MinHeapify(i);
+            for (int j = length/2 - 1; j >= 0; j--)
+            {
+                MinHeapify(j);
+            }
+        }
+        else {
+            cout << "Student Not Found";
+        }
+        return array;
+    }
+    void SortHeap(){
+        int SortedL = -1;
+        int L = length;
+        auto* SortedStudents = new Student[length];
+        for (int i = 0; i < L; ++i) {
+            SortedStudents[++SortedL] = array[0];
+            RemoveStudent(array[0].id);
+        }
+        for (int i = 0; i <= SortedL; ++i) {
+            array[i] = SortedStudents[i];
+            length++;
+        }
+    }
+    void Dep_Report() const{
+        map<string, int> Dep_Stud;
+        for (int i = 0; i < length; ++i) {
+            Dep_Stud[array[i].department]++;
+        }
+        for (auto & i : Dep_Stud) {
+            cout << i.first << " Department Has " << i.second << " Students\n";
+        }
+    }
+    void Print(){
+        SortHeap();
+        for (int i = 0; i < length; ++i){
+            cout << array[i] << '\n';
+        }
+    }
+};
+
+
+#endif //ASSIGNMENT_3_MINHEAP_H
